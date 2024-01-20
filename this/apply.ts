202c7a09aa59865a64c0a19f7f292309be6c8902
Tiar <tianruo.tian@hand-china.com>
2024-01-20 22:30:19 +0800

    add: this

---
+++ this/apply.ts
@@ -0,0 +1,18 @@
+Function.prototype['myApply'] = function (context, arguments) {
+  // 判断调用对象是否为函数
+  if (typeof context !== 'function') {
+    throw Error('The call must come from a function');
+  }
+
+  context = context || window;
+  // 创建一个唯一的属性以避免覆盖原有属性
+  const fnSymbol = Symbol();
+  context['fnSymbol'] = this;
+
+  const result = context['fnSymbol'](arguments);
+
+  // 删除添加的属性
+  delete context[fnSymbol];
+
+  return result;
+};
---
+++ this/bind.ts
@@ -0,0 +1,23 @@
+Function.prototype['myBind'] = function (context, ...arguments) {
+  if (typeof context !== 'function') {
+    throw Error('The call must come from a function');
+  }
+  const fn = this;
+
+  const bindFn = function (...callArgs) {
+    // 作为构造函数调用时，应该忽略显式绑定的 this 值
+    if (this instanceof bindFn) {
+      return fn.apply(this, [...arguments, ...callArgs]);
+    } else {
+      return fn.call(context, arguments);
+    }
+  };
+
+  // 将原始函数 fn 的原型链复制到 bindFn.prototype
+  // 那么通过 new bindFn() 创建的对象就能够正确地继承原始函数的原型链
+  if (fn.prototype) {
+    bindFn.prototype = Object.create(fn.prototype);
+  }
+
+  return bindFn;
+};
