d0031ffbe98442d783f019e03e9ddc4711f74a9e
Tiar <tianruo.tian@hand-china.com>
2024-01-23 21:10:16 +0800

    feat: Promsie链式调用

--- promise/index.tsx
+++ promise/index.tsx
@@ -54,6 +54,77 @@ function MyPromise(fn) {
   }
 }
 
+const resolvePromise = (promise, result, resolve, reject) => {
+  /**
+   * 处理循环引用的情况, 如:
+   * let promise = new MyPromise((resolve, reject) => {
+   * resolve(123);
+   * });
+   * let promise2 = promise.then(() => {
+   * return promise2; // 这里返回了 promise2 自身
+   * });
+   */
+  if (promise === result) {
+    return reject(new TypeError('Promise cannot resolve itself'));
+  }
+
+  let thenCalledOrThrow = false; // 判断否已经执行了 onfulfilled 或 onrejected
+
+  /**
+   * 通常指的是一个对象或函数，它有一个then方法，这个方法的行为类似于 Promise 的 then 方法。
+   * 这意味着虽然这个对象可能不是一个真正的 Promise 实例，但它遵循与 Promise 相似的接口和行为模式。
+   * 在 JavaScript中，这种类型的对象被称为 "thenable"
+   */
+  let thenable;
+
+  // 处理返回的 Promise：如果 result 是一个 Promise，我们需要等待它解析，然后根据它的结果来解析或拒绝 promise
+  if (result instanceof MyPromise) {
+    if (result.status === PENDING) {
+      // 如果 Promise 状态仍未确定，即还可能改变
+      result.then((value) => {
+        resolvePromise(promise, value, resolve, reject);
+      }, reject);
+    } else {
+      // 如果 Promise 状态已确定，直接采用它的状态
+      result.then(resolve, reject);
+    }
+    return;
+  }
+  // 处理 thenable 对象：如果 result 是一个具有 .then 方法的对象或函数，我们尝试以 Promise-like 的方式处理它
+  if (
+    (typeof result === 'function' || typeof result === 'object') &&
+    typeof result !== null
+  ) {
+    try {
+      thenable = result.then;
+      if (typeof thenable === 'function') {
+        thenable.call(
+          result,
+          (value) => {
+            if (thenCalledOrThrow) return;
+            thenCalledOrThrow = true;
+            return resolvePromise(promise, value, resolve, reject);
+          },
+          (reason) => {
+            if (thenCalledOrThrow) return;
+            thenCalledOrThrow = true;
+            return reject(reason);
+          }
+        );
+      } else {
+        resolve(result);
+      }
+    } catch (error) {
+      if (thenCalledOrThrow) return;
+      thenCalledOrThrow = true;
+      return reject(error);
+    }
+  } else {
+    resolve(result);
+  }
+  // 处理普通值和错误：如果 result 是普通值，我们直接解析 promise。如果在处理过程中发生错误，我们拒绝 promise
+};
+
 MyPromise.prototype.then = function (onfulfilled, onrejected) {
   onfulfilled =
     typeof onfulfilled === 'function' ? onfulfilled : (data) => data;
@@ -64,12 +135,13 @@ MyPromise.prototype.then = function (onfulfilled, onrejected) {
           throw error;
         };
 
+  let promise;
   if (this.status === PENDING) {
-    return new MyPromise((resolve, reject) => {
+    promise = new MyPromise((resolve, reject) => {
       this.onFulfilledFns.push(() => {
         try {
           let result = onfulfilled(this.value);
-          resolve(result);
+          resolvePromise(promise, result, resolve, reject);
         } catch (error) {
           reject(error);
         }
@@ -85,11 +157,11 @@ MyPromise.prototype.then = function (onfulfilled, onrejected) {
     });
   }
   if (this.status === FULFILLED) {
-    return new MyPromise((resolve, reject) => {
+    promise = new MyPromise((resolve, reject) => {
       setTimeout(() => {
         try {
           let result = onfulfilled(this.value);
-          resolve(result);
+          resolvePromise(promise, result, resolve, reject);
         } catch (error) {
           reject(error);
         }
@@ -97,43 +169,65 @@ MyPromise.prototype.then = function (onfulfilled, onrejected) {
     });
   }
   if (this.status === REJECT) {
-    return new MyPromise((resolve, reject) => {
+    promise = new MyPromise((resolve, reject) => {
       setTimeout(() => {
         try {
           let reason = onrejected(this.value);
-          resolve(reason);
+          resolvePromise(promise, reason, resolve, reject);
         } catch (error) {
           reject(error);
         }
       });
     });
   }
+
+  return promise;
 };
 
-// test code
-let pms = new MyPromise((resolve, reject) => {
+// // test code
+// let pms = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('hello');
+//   }, 1000);
+// });
+
+// // 连续调用
+// pms.then(() => {
+//   console.log('1');
+// });
+
+// pms.then(() => {
+//   console.log('2');
+// });
+
+// // 链式调用
+// pms
+//   .then(() => {
+//     console.log('1');
+//     return '2 next then';
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+// console.log('代码应该比 promise.then 先输出');
+
+// 显示返回一个 promise 实例
+const pms2 = new MyPromise((resolve) => {
   setTimeout(() => {
-    resolve('hello');
+    resolve('world');
   }, 1000);
 });
 
-// 连续调用
-pms.then(() => {
-  console.log('1');
-});
-
-pms.then(() => {
-  console.log('2');
-});
-
-// 链式调用
-pms
-  .then(() => {
-    console.log('1');
-    return '2 next then';
+pms2
+  .then((data) => {
+    console.log(data);
+    return new MyPromise((resolve) => {
+      setTimeout(() => {
+        resolve('world2 next then');
+      }, 1000);
+    });
   })
-  .then((res) => {
-    console.log(res);
+  .then((data) => {
+    console.log(data);
   });
-
-console.log('代码应该比 promise.then 先输出');
